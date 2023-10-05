5f4e646fb1c3d4268b270990abd57708488a60e0
Prakash Vasavda <prakash.v.php@gmail.com>
2023-10-05 20:45:00 +0530

    Header changes

--- resources/views/user/header.blade.php
+++ resources/views/user/header.blade.php
@@ -9,8 +9,22 @@
                     <ul>
                         <li class="{{ request()->is('/') ? 'active' : '' }}"><a href="{{ route('home') }}">home</a></li>
                         <li><a href="#">about US</a></li>
-                        <li class="{{ request()->is('candidates') ? 'active' : '' }}"><a href="{{ route('candidates') }}">candidates</a></li>
-                        <li class="{{ request()->is('families') ? 'active' : '' }} {{ request()->is('family-register') || request()->is('candidate-detail/*') ? 'active' : '' }}"><a href="{{ route('families') }}">family</a></li>
+                        @if(!session()->has('frontUser'))
+                            <li class="{{ request()->is('candidates') ? 'active' : '' }}"><a href="{{ route('candidates') }}">candidates</a></li>
+                            <li class="{{ request()->is('families') ? 'active' : '' }} {{ request()->is('family-register') || request()->is('candidate-detail/*') ? 'active' : '' }}"><a href="{{ route('families') }}">family</a></li>
+                        @elseif(session()->get('frontUser')->role != "family")
+                            <li class="#"><a href="#">Manage Profile</a></li>
+                            <li class="#"><a href="#">View Families</a></li>
+                            <li class="#"><a href="#">Manage Calander</a></li>
+                            <li class="#"><a href="#">Messages</a></li>
+                            <li class="#"><a href="#">Reviews</a></li>
+                        @else
+                            <li class="#"><a href="#">Manage Profile</a></li>
+                            <li class="#"><a href="#">Manage Candidates</a></li>
+                            <li class="#"><a href="#">Messages</a></li>
+                            <li class="#"><a href="#">Reviews</a></li>
+                            <li class="#"><a href="#">Manage Payments</a></li>
+                        @endif
                         <li class="{{ request()->is('contact-us') ? 'active' : '' }}"><a href="{{ route('contact-us') }}">Contact us</a></li>
                         @if(session()->has('frontUser') && session()->get('frontUser')->role == "family")
                             <li class="{{ request()->is('pricing') ? 'active' : '' }}"><a href="{{ route('pricing') }}">Pricing</a></li>
