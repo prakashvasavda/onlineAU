9254d29d07a78ec15b729da6e65750aef0ea138d
jatinbphp <jatin.b.php@gmail.com>
2023-10-26 20:06:13 +0530

    displaying favorite and all candidate at view candidate page

--- resources/views/user/candidate/view_candidates.blade.php
+++ resources/views/user/candidate/view_candidates.blade.php
@@ -16,71 +16,147 @@
 			</div>
 
 			<div class="title-main mb-5">
-	            <h2>All Candidates</h2>
+	            <h2>Favorites</h2>
 	        </div>
 
 			<div class="row result-list">
 				@if(isset($candidates) && !empty($candidates))
 					@foreach($candidates as $key => $value)
-						<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
-							<a href="{{ route('candidate-detail', ['id' => $value->id]) }}">
-								<div class="card">
-								    <div class="row g-0">
-								        <div class="col-md-4">
-								        	@if(isset($value->profile))
-								            	<img src="{{ url('../storage/app/public/uploads/'.$value->profile) }}" alt="">
-								            @else
-								            	<img src="{{ url('../storage/app/public/uploads/user-profile.png') }}" alt="">
-								            @endif
-								        </div>
-								        <div class="col-md-8">
-								            <div class="card-body">
-								            	<div class="pos-icon">
-								            		<div class="spinner-border" role="status" id="spinner{{$value->id}}" style="display: none;">
-													  <span class="visually-hidden">Loading...</span>
-													</div>
+						@if(isset($value->candidate_favorited_by) && is_string($value->candidate_favorited_by))
+							@if(in_array($user->id, explode(",", $value->candidate_favorited_by)))
+								<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+									<a href="{{ route('candidate-detail', ['id' => $value->id]) }}">
+										<div class="card">
+										    <div class="row g-0">
+										        <div class="col-md-4">
+										        	@if(isset($value->profile))
+										            	<img src="{{ url('../storage/app/public/uploads/'.$value->profile) }}" alt="">
+										            @else
+										            	<img src="{{ url('../storage/app/public/uploads/user-profile.png') }}" alt="">
+										            @endif
+										        </div>
+										        <div class="col-md-8">
+										            <div class="card-body">
+										            	<div class="pos-icon">
+										            		<div class="spinner-border" role="status" id="spinner{{$value->id}}" style="display: none;">
+															  <span class="visually-hidden">Loading...</span>
+															</div>
 
-								            		@if(isset($value->candidate_favorited_by) && is_string($value->candidate_favorited_by))
-								            			@if(in_array($user->id, explode(",", $value->candidate_favorited_by)))
-								            				<i class="fa-solid fa-heart" id="favBtn{{$value->id}}" onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
-								            			@else
-								            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
-								            			@endif
-								            		@else
-								            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
-								            		@endif
-								            	</div>
-								                <h5 class="card-title">{{ $value->name }}</h5>
-								                <p class="card-text">{{ $value->area }}</p>
-								                <p class="card-text">
-								                	<small>
-								                		<span>
-								                			@if(isset($value->review_rating_count) && is_string($value->review_rating_count))
-										                	 	@for($i = 0; $i < 5; $i++)
-															        @if($i < max(explode(",", $value->review_rating_count)))
-															            <i class="fa-solid fa-star"></i>
-															        @else
-															            <i class="fa-regular fa-star"></i>
-															        @endif
-														   	 	@endfor
-												            @else
-												            	@for($i=0; $i<5; $i++)
-												                	<i class="fa-regular fa-star"></i>
-												                @endfor
-											                @endif 
-									                	</span>
-									                	<span>{{ isset($value->total_reviews) ? $value->total_reviews : 0 }} Reviews</span>
-									                </small>
-									            </p>
-								            </div>
-								        </div>
-								    </div>
+										            		@if(isset($value->candidate_favorited_by) && is_string($value->candidate_favorited_by))
+										            			@if(in_array($user->id, explode(",", $value->candidate_favorited_by)))
+										            				<i class="fa-solid fa-heart" id="favBtn{{$value->id}}" onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+										            			@else
+										            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+										            			@endif
+										            		@else
+										            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+										            		@endif
+										            	</div>
+										                <h5 class="card-title">{{ $value->name }}</h5>
+										                <p class="card-text">{{ $value->area }}</p>
+										                <p class="card-text">
+										                	<small>
+										                		<span>
+										                			@if(isset($value->review_rating_count) && is_string($value->review_rating_count))
+												                	 	@for($i = 0; $i < 5; $i++)
+																	        @if($i < max(explode(",", $value->review_rating_count)))
+																	            <i class="fa-solid fa-star"></i>
+																	        @else
+																	            <i class="fa-regular fa-star"></i>
+																	        @endif
+																   	 	@endfor
+														            @else
+														            	@for($i=0; $i<5; $i++)
+														                	<i class="fa-regular fa-star"></i>
+														                @endfor
+													                @endif 
+											                	</span>
+											                	<span>{{ isset($value->total_reviews) ? $value->total_reviews : 0 }} Reviews</span>
+											                </small>
+											            </p>
+										            </div>
+										        </div>
+										    </div>
+										</div>
+									</a>
 								</div>
-							</a>
-						</div>
+							@endif
+						@endif
 					@endforeach
 				@endif
 			</div>
+
+
+			<div class="title-main mb-5">
+	            <h2>All Candidates</h2>
+	        </div>
+
+	        <div class="row result-list">
+				@if(isset($candidates) && !empty($candidates))
+					@foreach($candidates as $key => $value)
+						@if(!isset($value->candidate_favorited_by) || !in_array($user->id, explode(",", $value->candidate_favorited_by)))
+							<div class="col-lg-4 col-md-6 col-sm-12 col-xs-12">
+								<a href="{{ route('candidate-detail', ['id' => $value->id]) }}">
+									<div class="card">
+									    <div class="row g-0">
+									        <div class="col-md-4">
+									        	@if(isset($value->profile))
+									            	<img src="{{ url('../storage/app/public/uploads/'.$value->profile) }}" alt="">
+									            @else
+									            	<img src="{{ url('../storage/app/public/uploads/user-profile.png') }}" alt="">
+									            @endif
+									        </div>
+									        <div class="col-md-8">
+									            <div class="card-body">
+									            	<div class="pos-icon">
+									            		<div class="spinner-border" role="status" id="spinner{{$value->id}}" style="display: none;">
+														  <span class="visually-hidden">Loading...</span>
+														</div>
+
+									            		@if(isset($value->candidate_favorited_by) && is_string($value->candidate_favorited_by))
+									            			@if(in_array($user->id, explode(",", $value->candidate_favorited_by)))
+									            				<i class="fa-solid fa-heart" id="favBtn{{$value->id}}" onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+									            			@else
+									            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+									            			@endif
+									            		@else
+									            				<i class="fa-regular fa-heart" id="favBtn{{$value->id}}"  onclick="storeFamilyFavoriteCandidate(event, '{{ $value->id }}')"></i>
+									            		@endif
+									            	</div>
+									                <h5 class="card-title">{{ $value->name }}</h5>
+									                <p class="card-text">{{ $value->area }}</p>
+									                <p class="card-text">
+									                	<small>
+									                		<span>
+									                			@if(isset($value->review_rating_count) && is_string($value->review_rating_count))
+											                	 	@for($i = 0; $i < 5; $i++)
+																        @if($i < max(explode(",", $value->review_rating_count)))
+																            <i class="fa-solid fa-star"></i>
+																        @else
+																            <i class="fa-regular fa-star"></i>
+																        @endif
+															   	 	@endfor
+													            @else
+													            	@for($i=0; $i<5; $i++)
+													                	<i class="fa-regular fa-star"></i>
+													                @endfor
+												                @endif 
+										                	</span>
+										                	<span>{{ isset($value->total_reviews) ? $value->total_reviews : 0 }} Reviews</span>
+										                </small>
+										            </p>
+									            </div>
+									        </div>
+									    </div>
+									</div>
+								</a>
+							</div>
+						@endif
+					@endforeach
+				@endif
+			</div>
+
+
 		</div>
 	</div>
 </div>
