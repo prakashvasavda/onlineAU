69ccaa78d7456f669d8769db8a020f8e187b13e1
jatinbphp <jatin.b.php@gmail.com>
2023-11-03 20:35:05 +0530

    fixing search issue

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -17,11 +17,12 @@ class SearchController extends Controller
         }
         
         if ($data['type'] == "family") {
-            $search = FrontUser::leftJoin('candidate_favorite_families', 'front_users.id', '=', 'candidate_favorite_families.family_id')
+            $search = FrontUser::leftJoin(DB::raw('(SELECT family_id, GROUP_CONCAT(DISTINCT candidate_id) as candidate_favorite_families FROM candidate_favorite_families GROUP BY family_id) as candidate_favorites'), 'front_users.id', '=', 'candidate_favorites.family_id')
             ->leftJoin('family_reviews', 'front_users.id', '=', 'family_reviews.family_id')
+            ->leftJoin('family_favorite_candidates', 'front_users.id', '=', 'family_favorite_candidates.family_id')
             ->select(
                 'front_users.*',
-                'candidate_favorite_families.candidate_id AS candidate_favourite_family',
+                'candidate_favorites.candidate_favorite_families AS family_favorited_by', //holds the ids of candidate who have liked this family from result set
                 'reviews.review_note',
                 'reviews.review_rating_count',
                 'reviews.total_reviews'
@@ -29,15 +30,15 @@ class SearchController extends Controller
             ->leftJoin(DB::raw('(SELECT family_id, GROUP_CONCAT(DISTINCT review_note) as review_note, GROUP_CONCAT(DISTINCT review_rating_count) as review_rating_count, COUNT(DISTINCT id) as total_reviews FROM family_reviews GROUP BY family_id) as reviews'), 'front_users.id', '=', 'reviews.family_id')
             ->where('front_users.role', 'family')
             ->where('front_users.status', '1')
-            ->whereRaw("family_address LIKE ?", ['%' . $data['search'] . '%'])
+            ->where('front_users.area', 'like', '%'.$data['search'].'%')
             ->distinct()
             ->get();
         } else {
-            $search = FrontUser::leftJoin('family_favorite_candidates', 'front_users.id', '=', 'family_favorite_candidates.candidate_id')
+            $search = FrontUser::leftJoin(DB::raw('(SELECT candidate_id, GROUP_CONCAT(DISTINCT family_id) as family_favorite_candidate FROM family_favorite_candidates GROUP BY candidate_id) as family_favorites'), 'front_users.id', '=', 'family_favorites.candidate_id')
             ->leftJoin('candidate_reviews', 'front_users.id', '=', 'candidate_reviews.candidate_id')
             ->select(
                 'front_users.*',
-                'family_favorite_candidates.candidate_id AS family_favorite_candidate',
+                'family_favorites.family_favorite_candidate AS candidate_favorited_by', //return the ids of the families who liked this candidate
                 'reviews.review_note',
                 'reviews.review_rating_count',
                 'reviews.total_reviews'
@@ -45,10 +46,11 @@ class SearchController extends Controller
             ->leftJoin(DB::raw('(SELECT candidate_id, GROUP_CONCAT(DISTINCT review_note) as review_note, GROUP_CONCAT(DISTINCT review_rating_count) as review_rating_count, COUNT(DISTINCT id) as total_reviews FROM candidate_reviews GROUP BY candidate_id) as reviews'), 'front_users.id', '=', 'reviews.candidate_id')
             ->where('front_users.role', '!=', 'family')
             ->where('front_users.status', '1')
-            ->whereRaw("front_users.area LIKE ?", ['%' . $data['search'] . '%'])
+            ->where('front_users.area', 'like', '%'.$data['search'].'%')
             ->distinct()
             ->get();
         }
+
         $type = $data['type'];
         return view('user.search', compact('search', 'type'));
     }
