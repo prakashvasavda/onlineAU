27ea703d3da88aaaf8046843279a9cc2655a1750
jatinbphp <jatin.b.php@gmail.com>
2023-11-07 20:39:13 +0530

    spliting name in two fields

--- resources/views/family/edit.blade.php
+++ resources/views/family/edit.blade.php
@@ -43,7 +43,7 @@
 
                                  <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                     <div class="form-group">
-                                        <label for="name">Full Name <span class="text-danger">*</span></label>
+                                        <label for="name">Name <span class="text-danger">*</span></label>
                                         <input type="text" id="name" name="name" placeholder="" class="form-control"  value="{{ old('name', isset($family->name) ? $family->name : '') }}">
                                          @if ($errors->has('name'))
                                             <span class="text-danger">
