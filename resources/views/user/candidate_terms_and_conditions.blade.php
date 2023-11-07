550f286dfaeabe8beafb3f011741316c25f0d62a
jatinbphp <jatin.b.php@gmail.com>
2023-11-07 13:17:50 +0530

    adding tc files

--- resources/views/user/candidate_terms_and_conditions.blade.php
+++ resources/views/user/candidate_terms_and_conditions.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
   <title>Terms and Conditions</title>
@@ -16,43 +16,56 @@
     }
   </style>
 </head>
-<body>
-  <h1>Terms and Conditions</h1>
-  <p>Please read these terms and conditions carefully before using our services as a candidate on our platform. By accessing our website and using our services, you agree to abide by the following terms and conditions:</p>
+<body> -->
+@extends('layouts.main')
 
-  <ol>
-    <li>
-      <p><strong>Eligibility:</strong> To use our platform and become a candidate, you must be at least 18 years of age and legally eligible to work in the country of your placement. You are responsible for ensuring that you meet all legal requirements for the desired position.</p>
-    </li>
-    <li>
-      <p><strong>Registration and Profile:</strong> To apply for positions, you must complete the registration process and create a profile on our platform. You are responsible for providing accurate and up-to-date information in your profile, including personal, educational, and contact information.</p>
-    </li>
-    <li>
-      <p><strong>Placement Responsibility:</strong> Our company connects candidates with families. We do not act as an employer or agent for either party. The terms of employment, responsibilities, working conditions, and compensation will be determined by the family and the candidate. We do not take responsibility for any disputes, conflicts, or issues that may arise between the candidate and the family.</p>
-    </li>
-    <li>
-      <p><strong>Safety and Security:</strong> While we strive to maintain a safe platform, we do not take responsibility for the safety of the candidates. It is your responsibility to assess the suitability of a family, communicate your expectations, and negotiate the terms of your position. We recommend that you conduct background checks and interviews with potential families to ensure your safety.</p>
-    </li>
-    <li>
-      <p><strong>Liability:</strong> We accept no liability for any injuries, damages, losses, or claims that may occur during your employment with a family or while using our services. You agree to indemnify and hold us harmless from any claims, damages, or liabilities resulting from your interactions with families or your use of our platform.</p>
-    </li>
-    <li>
-      <p><strong>Code of Conduct:</strong> As an Au-Pair candidate, you must adhere to a high standard of conduct, respecting the laws and regulations of your country. Any illegal activities or violation of the terms and conditions may result in the termination of your account and disqualification from our platform.</p>
-    </li>
-    <li>
-      <p><strong>Privacy:</strong> We take your privacy seriously and will handle your personal information in accordance with our Privacy Policy. By using our platform, you consent to the collection, use, and storage of your personal data.</p>
-    </li>
-    <li>
-      <p><strong>Termination:</strong> We reserve the right to terminate your account and access to our platform at our discretion, without prior notice, if you violate these terms and conditions or engage in any behavior that we consider detrimental to our platform or the Childcare community.</p>
-    </li>
-    <li>
-      <p><strong>Amendments:</strong> We may update these terms and conditions from time to time. You will be notified of any changes, and it is your responsibility to review and accept the updated terms to continue using our services.</p>
-    </li>
-    <li>
-      <p><strong>Governing Law:</strong> These terms and conditions are governed by the laws of South Africa. Any disputes or claims will be subject to the exclusive jurisdiction of the courts of South Africa.</p>
-    </li>
-  </ol>
+@section('content')
+<div class="simple-content-main no-banner py-5">
+  <div class="container">
+    <div class="title-main">
+      <h2 class="text-center mb-4">Terms and Conditions</h2>
+    </div>
 
-  <p>By using our platform, you acknowledge that you have read and understood these terms and conditions and agree to be bound by them. If you do not agree with any part of these terms, please do not use our services.</p>
-</body>
-</html>
+    <p class="small mb-1">Please read these terms and conditions carefully before using our services as a candidate on our platform. By accessing our website and using our services, you agree to abide by the following terms and conditions:</p>
+    <ol class="mb-2">
+      <li class="mb-1">
+        <p class="small"><strong>Eligibility:</strong> To use our platform and become a candidate, you must be at least 18 years of age and legally eligible to work in the country of your placement. You are responsible for ensuring that you meet all legal requirements for the desired position.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Registration and Profile:</strong> To apply for positions, you must complete the registration process and create a profile on our platform. You are responsible for providing accurate and up-to-date information in your profile, including personal, educational, and contact information.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Placement Responsibility:</strong> Our company connects candidates with families. We do not act as an employer or agent for either party. The terms of employment, responsibilities, working conditions, and compensation will be determined by the family and the candidate. We do not take responsibility for any disputes, conflicts, or issues that may arise between the candidate and the family.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Safety and Security:</strong> While we strive to maintain a safe platform, we do not take responsibility for the safety of the candidates. It is your responsibility to assess the suitability of a family, communicate your expectations, and negotiate the terms of your position. We recommend that you conduct background checks and interviews with potential families to ensure your safety.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Liability:</strong> We accept no liability for any injuries, damages, losses, or claims that may occur during your employment with a family or while using our services. You agree to indemnify and hold us harmless from any claims, damages, or liabilities resulting from your interactions with families or your use of our platform.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Code of Conduct:</strong> As an Au-Pair candidate, you must adhere to a high standard of conduct, respecting the laws and regulations of your country. Any illegal activities or violation of the terms and conditions may result in the termination of your account and disqualification from our platform.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Privacy:</strong> We take your privacy seriously and will handle your personal information in accordance with our Privacy Policy. By using our platform, you consent to the collection, use, and storage of your personal data.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Termination:</strong> We reserve the right to terminate your account and access to our platform at our discretion, without prior notice, if you violate these terms and conditions or engage in any behavior that we consider detrimental to our platform or the Childcare community.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Amendments:</strong> We may update these terms and conditions from time to time. You will be notified of any changes, and it is your responsibility to review and accept the updated terms to continue using our services.</p>
+      </li>
+      <li class="mb-1">
+        <p class="small"><strong>Governing Law:</strong> These terms and conditions are governed by the laws of South Africa. Any disputes or claims will be subject to the exclusive jurisdiction of the courts of South Africa.</p>
+      </li>
+    </ol>
+    <p class="small">By using our platform, you acknowledge that you have read and understood these terms and conditions and agree to be bound by them. If you do not agree with any part of these terms, please do not use our services.</p>
+  </div>
+</div>
+
+@endsection
+@section('script')
+@parent
+@endsection
+<!-- </body>
+</html> -->
--- resources/views/user/family_terms_and_conditions.blade.php
+++ resources/views/user/family_terms_and_conditions.blade.php
@@ -1,8 +1,7 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html>
 <head>
     <title>Terms and Conditions</title>
-    
       <style>
     body {
       font-family: Arial, sans-serif;
@@ -17,57 +16,69 @@
     }
   </style>
 </head>
-<body>
+<body> -->
+@extends('layouts.main')
 
-<h1>Terms and Conditions</h1>
-<p>This document sets forth the terms and conditions governing the services provided by Online Au-Pairs (hereinafter referred to as "the Company") to families seeking childcare services in South Africa. By engaging the services of the Company, families agree to abide by these terms and conditions.</p>
+@section('content')
+<div class="simple-content-main no-banner py-5">
+  <div class="container">
+    <div class="title-main">
+      <h2 class="text-center mb-4">Terms and Conditions</h2>
+    </div>
 
-<p>The term "candidate" in the following Terms and Conditions refers to any Au-Pair, Nanny, Babysitter, or Pet Sitter that is registered with Online Au-Pairs.</p>
+    <p class="mb-2">This document sets forth the terms and conditions governing the services provided by Online Au-Pairs (hereinafter referred to as "the Company") to families seeking childcare services in South Africa. By engaging the services of the Company, families agree to abide by these terms and conditions.</p>
+    <p class="mb-1">The term "candidate" in the following Terms and Conditions refers to any Au-Pair, Nanny, Babysitter, or Pet Sitter that is registered with Online Au-Pairs.</p>
 
-<h2>1) Introduction</h2>
-<p>a) The Company offers placement services for families in South Africa seeking childcare support.</p>
-<p>b) Families using our services must adhere to these terms and conditions.</p>
+    <h2 class="h4 mt-4 mb-2">1) Introduction</h2>
+    <p class="small mb-1">a) The Company offers placement services for families in South Africa seeking childcare support.</p>
+    <p class="small mb-1">b) Families using our services must adhere to these terms and conditions.</p>
 
-<h2>2) Service Description</h2>
-<p>a) The Company will conduct a thorough screening of candidates to match them with suitable families.</p>
-<p>b) The Company will provide families with profiles of potential candidates based on their specified criteria.</p>
-<p>c) The Company will facilitate interviews and assist in the selection process.</p>
-<p>d) The Company will guide families in the negotiation and establishment of the candidate's terms of engagement.</p>
+    <h2 class="h4 mt-4 mb-2">2) Service Description</h2>
+    <p class="small mb-1">a) The Company will conduct a thorough screening of candidates to match them with suitable families.</p>
+    <p class="small mb-1">b) The Company will provide families with profiles of potential candidates based on their specified criteria.</p>
+    <p class="small mb-1">c) The Company will facilitate interviews and assist in the selection process.</p>
+    <p class="small mb-1">d) The Company will guide families in the negotiation and establishment of the candidate's terms of engagement.</p>
 
-<h2>3) Fees</h2>
-<p>a) Families will be charged a placement fee for the service, as outlined in the fee schedule provided.</p>
-<p>b) All fees are non-refundable.</p>
-<p>c) The Company offers a 3-month warranty, which means that if the candidate's contract is terminated within the first 3 months after employment, the Company will be responsible for providing the family with a replacement. This warranty will only be valid if the family pays their placement fee within 7 working days after receiving the invoice.</p>
+    <h2 class="h4 mt-4 mb-2">3) Fees</h2>
+    <p class="small mb-1">a) Families will be charged a placement fee for the service, as outlined in the fee schedule provided.</p>
+    <p class="small mb-1">b) All fees are non-refundable.</p>
+    <p class="small mb-1">c) The Company offers a 3-month warranty, which means that if the candidate's contract is terminated within the first 3 months after employment, the Company will be responsible for providing the family with a replacement. This warranty will only be valid if the family pays their placement fee within 7 working days after receiving the invoice.</p>
 
-<h2>4) Candidate Selection</h2>
-<p>a) The final decision to hire a candidate rests with the family.</p>
-<p>b) The Company will not be held responsible for the conduct, actions, or performance of the selected candidate.</p>
+    <h2 class="h4 mt-4 mb-2">4) Candidate Selection</h2>
+    <p class="small mb-1">a) The final decision to hire a candidate rests with the family.</p>
+    <p class="small mb-1">b) The Company will not be held responsible for the conduct, actions, or performance of the selected candidate.</p>
 
-<h2>5) Candidate Terms of Engagement</h2>
-<p>a) Families are responsible for establishing the terms and conditions of the candidate's employment, including salary, working hours, and duties.</p>
-<p>b) Families must comply with all relevant South African labor laws.</p>
+    <h2 class="h4 mt-4 mb-2">5) Candidate Terms of Engagement</h2>
+    <p class="small mb-1">a) Families are responsible for establishing the terms and conditions of the candidate's employment, including salary, working hours, and duties.</p>
+    <p class="small mb-1">b) Families must comply with all relevant South African labor laws.</p>
 
-<h2>6) Candidate Insurance</h2>
-<p>a) The Company does not provide insurance coverage for candidates.</p>
+    <h2 class="h4 mt-4 mb-2">6) Candidate Insurance</h2>
+    <p class="small mb-1">a) The Company does not provide insurance coverage for candidates.</p>
 
-<h2>7) Candidate's Visa and Permits</h2>
-<p>a) Families are responsible for ensuring that their employee (candidate) has the necessary visas and permits to work in South Africa.</p>
-<p>b) The Company may offer guidance on the visa application process, but the responsibility ultimately rests with the family.</p>
+    <h2 class="h4 mt-4 mb-2">7) Candidate's Visa and Permits</h2>
+    <p class="small mb-1">a) Families are responsible for ensuring that their employee (candidate) has the necessary visas and permits to work in South Africa.</p>
+    <p class="small mb-1">b) The Company may offer guidance on the visa application process, but the responsibility ultimately rests with the family.</p>
 
-<h2>8) Termination of Services</h2>
-<p>a) Either party may terminate the services of the candidate at any time with reasonable notice.</p>
-<p>b) Families must inform the Company of any significant issues or concerns regarding the candidate.</p>
+    <h2 class="h4 mt-4 mb-2">8) Termination of Services</h2>
+    <p class="small mb-1">a) Either party may terminate the services of the candidate at any time with reasonable notice.</p>
+    <p class="small mb-1">b) Families must inform the Company of any significant issues or concerns regarding the candidate.</p>
 
-<h2>9) Confidentiality</h2>
-<p>a) Families must maintain the confidentiality of the personal information and profiles of candidates provided by the Company.</p>
+    <h2 class="h4 mt-4 mb-2">9) Confidentiality</h2>
+    <p class="small mb-1">a) Families must maintain the confidentiality of the personal information and profiles of candidates provided by the Company.</p>
 
-<h2>10) Governing Law</h2>
-<p>a) These terms and conditions are governed by the laws of South Africa.</p>
+    <h2 class="h4 mt-4 mb-2">10) Governing Law</h2>
+    <p class="small mb-1">a) These terms and conditions are governed by the laws of South Africa.</p>
 
-<h2>11) Amendment of Terms</h2>
-<p>a) The Company reserves the right to amend these terms and conditions at any time. Families will be informed of any changes.</p>
+    <h2 class="h4 mt-4 mb-2">11) Amendment of Terms</h2>
+    <p class="small mb-4">a) The Company reserves the right to amend these terms and conditions at any time. Families will be informed of any changes.</p>
 
-<p>By engaging the services of Online Au-Pairs, families acknowledge that they have read, understood, and agreed to these terms and conditions.</p>
+    <p>By engaging the services of Online Au-Pairs, families acknowledge that they have read, understood, and agreed to these terms and conditions.</p>
+  </div>
+</div>
 
-</body>
-</html>
+@endsection
+@section('script')
+@parent
+@endsection
+<!-- </body>
+</html> -->
