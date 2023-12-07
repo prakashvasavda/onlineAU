1bf728910bbb0285ef31f2d4b6f995f58e049f51
jatinbphp <jatin.b.php@gmail.com>
2023-12-07 18:35:45 +0530

    adding corresponding icons in the dashboard page

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,42 +5,56 @@
         <div class="container-fluid">
             <div class="row">
                 <div class="col-lg-3 col-6">
-                    <a href="{{url('/users')}}" class="small-box-footer">
+                    <a href="#" class="small-box-footer">
                         <div class="small-box bg-info">
                             <div class="inner">
-                                <h3>55</h3>
-                                <p>Total Families</p>
+                                <h3>57</h3>
+                                <p>Total Petisitters</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-users"></i>
+                                <i class="fa fa-dog"></i>
                             </div>
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-3 col-6">
-                    <a href="{{url('/stock_category')}}" class="small-box-footer">
+                    <a href="#" class="small-box-footer">
                         <div class="small-box bg-primary">
                             <div class="inner">
-                                <h3>10</h3>
-                                <p>Total Candidates</p>
+                                <h3>14</h3>
+                                <p>Total Nannies</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-sitemap"></i>
+                                <i class="fa fa-user-nurse"></i>
                             </div>
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-3 col-6">
-                    <a href="{{url('/stock')}}" class="small-box-footer">
+                    <a href="#" class="small-box-footer">
                         <div class="small-box bg-danger">
                             <div class="inner">
-                                <h3>50</h3>
-                                <p>Total Visits</p>
+                                <h3>3</h3>
+                                <p>Total Babysitters</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-th"></i>
+                                <i class="fa fa-baby"></i>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-3 col-6">
+                    <a href="#" class="small-box-footer">
+                        <div class="small-box bg-warning">
+                            <div class="inner">
+                                <h3>5</h3>
+                                <p>Total Au-Pairs</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-home"></i>
                             </div>
                         </div>
                     </a>
