6a440f47d12659e3c25921bdf757e4fe075d74d3
jatinbphp <jatin.b.php@gmail.com>
2023-12-07 16:50:36 +0530

    making changes in main layouts title

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,7 +6,7 @@
         <meta name="apple-mobile-web-app-capable" content="yes">
         <meta name="format-detection" content="telephone=no"/>
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Family</title>
+        <title>{{ config('app.name', 'ONLINE-AUPAIRS') }}</title>
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('front/images/favicon.ico') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
