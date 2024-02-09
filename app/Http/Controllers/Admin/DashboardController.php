119512525b99a3d1c43ec8ebbda7cfb7f6dc86c6
emmanuelkphp <emmanuel.k.php@gmail.com>
2024-02-09 11:49:48 +0530

    fetching front users in the dashboard page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,29 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
+use App\Models\FrontUser;
+
+
 
 class DashboardController extends Controller{
     
     public function index(){
-        $data['menu'] = 'Dashboard';
+
+        $front_users = FrontUser::selectRaw("
+            SUM(CASE WHEN role = 'au-pairs' THEN 1 ELSE 0 END) as total_aupairs,
+            SUM(CASE WHEN role = 'nannies' THEN 1 ELSE 0 END) as total_nannies,
+            SUM(CASE WHEN role = 'family' THEN 1 ELSE 0 END) as total_families,
+            SUM(CASE WHEN role = 'babysitters' THEN 1 ELSE 0 END) as total_babysitters,
+            SUM(CASE WHEN role = 'nannies' THEN 1 ELSE 0 END) as total_nannies,
+            SUM(CASE WHEN role = 'family-petsitting' THEN 1 ELSE 0 END) as total_family_petsittings,
+            SUM(CASE WHEN role = 'petsitters' THEN 1 ELSE 0 END) as total_petsitters
+
+        ")->first();
+
+        $data['menu']           = 'Dashboard';
+        $data['front_users']    = $front_users;
         return view('admin.dashboard',$data);
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,57 +4,85 @@
     <section class="content pt-3">
         <div class="container-fluid">
             <div class="row">
+                <div class="col-lg-6 col-6">
+                    <a href="{{ route('admin.families') }}" class="small-box-footer">
+                        <div class="small-box bg-white">
+                            <div class="inner">
+                                <h3>{{ isset($front_users->total_families) ? $front_users->total_families : "0" }}</h3>
+                                <p>Total Families</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-user"></i>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
+                <div class="col-lg-6 col-6">
+                    <a href="{{ route('admin.family-petsitting.index') }}" class="small-box-footer">
+                        <div class="small-box bg-dark">
+                            <div class="inner">
+                                <h3>{{ isset($front_users->total_family_petsittings) ? $front_users->total_family_petsittings : "0" }}</h3>
+                                <p>Total Family Petsitting</p>
+                            </div>
+                            <div class="icon">
+                                <i class="fa fa-user"></i>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+
                 <div class="col-lg-3 col-6">
-                    <a href="#" class="small-box-footer">
+                    <a href="{{ route('admin.candidates.petsitters') }}" class="small-box-footer">
                         <div class="small-box bg-info">
                             <div class="inner">
-                                <h3>57</h3>
+                                <h3>{{ isset($front_users->total_petsitters) ? $front_users->total_petsitters : "0" }}</h3>
                                 <p>Total Petisitters</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-dog"></i>
+                                <i class="fa fa-user"></i>
                             </div>
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-3 col-6">
-                    <a href="#" class="small-box-footer">
-                        <div class="small-box bg-primary">
+                    <a href="{{ route('admin.candidates.nannies') }}" class="small-box-footer">
+                        <div class="small-box bg-success">
                             <div class="inner">
-                                <h3>14</h3>
+                                <h3>{{ isset($front_users->total_nannies) ? $front_users->total_nannies : "0" }}</h3>
                                 <p>Total Nannies</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-user-nurse"></i>
+                                <i class="fa fa-user"></i>
                             </div>
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-3 col-6">
-                    <a href="#" class="small-box-footer">
+                    <a href="{{ route('admin.candidates.babysitters') }}" class="small-box-footer">
                         <div class="small-box bg-danger">
                             <div class="inner">
-                                <h3>3</h3>
+                                <h3>{{ isset($front_users->total_babysitters) ? $front_users->total_babysitters : "0" }}</h3>
                                 <p>Total Babysitters</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-baby"></i>
+                                <i class="fa fa-user"></i>
                             </div>
                         </div>
                     </a>
                 </div>
 
                 <div class="col-lg-3 col-6">
-                    <a href="#" class="small-box-footer">
+                    <a href="{{ route('admin.candidates.aupairs') }}" class="small-box-footer">
                         <div class="small-box bg-warning">
                             <div class="inner">
-                                <h3>5</h3>
+                                <h3>{{ isset($front_users->total_aupairs) ? $front_users->total_aupairs : "0" }}</h3>
                                 <p>Total Au-Pairs</p>
                             </div>
                             <div class="icon">
-                                <i class="fa fa-home"></i>
+                                <i class="fa fa-user"></i>
                             </div>
                         </div>
                     </a>
