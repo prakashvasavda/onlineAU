0723c25330684b3f3f45f1821888c9322a2fbbdf
emmanuelkphp <emmanuel.k.php@gmail.com>
2024-02-24 20:59:06 +0530

    adding required field in the petsiting form

--- resources/views/admin/aupairs/edit.blade.php
+++ resources/views/admin/aupairs/edit.blade.php
@@ -716,7 +716,7 @@
 
                                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                         <div class="mb-2">
-                                            <label for="day_hour">What are your available days and hours</label>
+                                            <label for="day_hour">What are your available days and hours <span class="text-danger">*</span></label>
                                         </div>
                                         {{-- calender --}}
                                         @include ('admin.calender.edit')
--- resources/views/admin/family/edit.blade.php
+++ resources/views/admin/family/edit.blade.php
@@ -512,7 +512,7 @@
                                 <div class="row">
                                     <div class="col-lg-6 col-md-6 col-sm-12 col-xs-12">
                                         <div class="mb-2">
-                                            <label for="day_hour">Days & times needed</label>
+                                            <label for="day_hour">Days & times needed <span class="text-danger">*</span></label>
                                         </div>
                                         {{-- calender --}}
                                         @include ('admin.calender.edit')
