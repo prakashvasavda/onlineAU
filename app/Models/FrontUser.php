cfff2fc902688f035e7dc96ad70b40e68a80c8b0
emmanuelkphp <emmanuel.k.php@gmail.com>
2024-03-13 19:05:40 +0530

    adding softdeletes in the frontuser table

--- app/Models/FrontUser.php
+++ app/Models/FrontUser.php
@@ -2,16 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 
 class FrontUser extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $guarded = [];
     protected $table   = 'front_users';
+    protected $dates = ['deleted_at'];
     protected $fillable = [
         'name',
         'age',
